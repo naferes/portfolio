4310ea5aa8fb23effd2c0a515d44bf06124aa085
CodexJE <codexjuniorentreprise@gmail.com>
2022-01-25 19:10:58 +0100

    warnings

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Feres Nasri',
     title: "Cloud/Devops Engineer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'http://9ec8-102-156-87-38.ngrok.io/isso.jpg',
+    image: 'https://flamboyant-goldberg-43f4ca.netlify.app/isso.jpg',
     resumePdf: resume
 }
