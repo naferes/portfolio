f0c251dc056fa5cf1c899ed8d0892ffd0f15f56e
CodexJE <codexjuniorentreprise@gmail.com>
2022-02-24 23:44:12 +0100

    new certificate

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -11,7 +11,7 @@ export const achievementData = {
             link: 'https://www.credly.com/badges/671a3226-5da5-4e16-a4d0-e219deab87e9'
         },
         {
-            id : 1,
+            id : 2,
             title : 'Oracle Cloud Infrastructure Foundations Associate',
             details : 'First ceritifcation needed to get the Oracle Cloud Infrastructure Professional Arhitect from Oracle',
             date : 'Jan 31, 2022',
@@ -27,6 +27,15 @@ export const achievementData = {
             field : 'Devops',
             image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Cisco_academy_logo.svg/1200px-Cisco_academy_logo.svg.png',
             link: 'https://www.credly.com/badges/a4d8701b-7a2f-45ef-9ce4-f8d4138f6f69'
+        },
+        {
+            id : 4,
+            title : 'Oracle Cloud Infrastructure Architect 2021 (Associate)',
+            details : 'Oficially certified as an oracle cloud infrastructure architect',
+            date : 'Feb 22, 2022',
+            field : 'Cloud',
+            image : 'https://freepikpsd.com/file/2020/01/Oracle-PNG-File.png',
+            link : 'https://catalog-education.oracle.com/pls/certview/sharebadge?id=30DE16A73B3A78AFB23ACE0A2E4736AE85FB67CCB25D85C960405CE934DF1FC6'
         },
 
     ]
