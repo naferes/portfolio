abc605aa0ff05576d2e742a842c50cbd7f097dad
feres <nasriferesd@gmail.com>
2022-06-08 14:32:26 +0100

    pdf update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     title: "Cloud/Devops Engineer",
     desciption:"“People tell you the world looks a certain way. Parents tell you how to think. Schools tell you how to think. TV. Religion. And then at a certain point, if you’re lucky, you realize you can make up your own mind. Nobody sets the rules but you. You can design your own life.”— Carrie Ann Moss ",
     image: 'https://flamboyant-goldberg-43f4ca.netlify.app/isso.jpg',
-    resumePdf: resume
+    resumePdf: Nasri_feres_CV
 }
